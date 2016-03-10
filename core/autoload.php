ee8588bbef04f435d51498093ff5e5d71f82d875
Alexander Vasiliev <alexander.vasiliev@postindustria.com>
2016-03-10 16:13:41 +0200

    refactor autoload

--- core/autoload.php
+++ core/autoload.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: alex
@@ -7,13 +8,20 @@
  */
 
 spl_autoload_register(function($class) {
-    $class = str_replace('_', '-', $class);
-    $path = Config::get('root');
-    $path .= '/'.str_replace('\\', '/', $class);
-    $path .= '.php';
+    $path = parseNameSpace($class);
     if (file_exists($path)) {
         require_once $path;
     } else {
         dd($class . ' not found');
     }
 });
+
+function parseNameSpace($class)
+{
+    $class = str_replace('_', '-', $class);
+    $path = Config::get('root');
+    $path .= '/'.str_replace('\\', '/', $class);
+    $path .= '.php';
+
+    return $path;
+}
